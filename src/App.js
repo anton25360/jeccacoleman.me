b7ff7e3654b5461719519f80bbc23903b5237d8d
Anton Webb <anton25360@gmail.com>
2020-09-25 16:25:15 +0100

    added video

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <h1 id='title'>You Smell</h1>
+      <h1 id="title">You Smell</h1>
+
+      <iframe
+        id='video'
+        width="560"
+        height="315"
+        src="https://www.youtube-nocookie.com/embed/PPzl7fnN15E"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowfullscreen
+      ></iframe>
     </div>
   );
 }
