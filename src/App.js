002453f3e3b27299e804a7cac3168f60b382818c
Anton Webb <anton25360@gmail.com>
2020-09-25 16:30:25 +0100

    added subtitle text

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <h1 id="title">You Smell</h1>
 
       <iframe
-        id='video'
+        id="video"
         width="560"
         height="315"
         src="https://www.youtube-nocookie.com/embed/PPzl7fnN15E"
@@ -16,6 +16,8 @@ function App() {
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
         allowfullscreen
       ></iframe>
+
+      <p id="subtitle">jk you're amazing & I love you ❤️</p>
     </div>
   );
 }
