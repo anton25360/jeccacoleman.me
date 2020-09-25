4a39403066f62b2e7c9601ce649fdc963313bd87
Anton Webb <anton25360@gmail.com>
2020-09-25 16:48:18 +0100

    Updates

---
+++ precache-manifest.8f9b4adb8cf0f28b3539ce752f996975.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "359703230bb0fe654f982f252c10f64e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c26ebfd8cede4c4fa4b1",
+    "url": "/static/css/main.763e3e1d.chunk.css"
+  },
+  {
+    "revision": "0aac57c81b76f5ea5511",
+    "url": "/static/js/2.cc7da1fe.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.cc7da1fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c26ebfd8cede4c4fa4b1",
+    "url": "/static/js/main.56c792b8.chunk.js"
+  },
+  {
+    "revision": "d018b5de928fab296e5b",
+    "url": "/static/js/runtime-main.dfb1f0ec.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
