1b4fc2540140096c88f865c956dae224ffa009c3
Anton Webb <anton25360@gmail.com>
2021-05-09 20:38:51 +0100

    Add intro section

--- src/App.js
+++ src/App.js
@@ -5,12 +5,30 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <div className='welcome-container'>
-        <div className='navbar'>
-          <p className='navbar-link'>Short Films</p>
-          <p className='navbar-link'>Commercial Work</p>
-          <p className='navbar-link'>Contact</p>
+      <div className="welcome-container">
+        <div className="navbar">
+          <p className="navbar-link">Short Films</p>
+          <p className="navbar-link">Commercial Work</p>
+          <p className="navbar-link">Contact</p>
         </div>
+        <div className="intro-container">
+          <h1 className="intro-title">Hi, I'm Jecca.</h1>
+          <p className="intro-desc">
+            I'm a second-year filmmaking student at the University of the West
+            of England (UWE) currently specialising in editing and colour
+            grading.
+            <br />
+            <br />
+            I have experience using Adobe Premiere Pro, AVID Media Composer,
+            Adobe After Effects, and Adobe Audition.
+            <br />
+            <br /> Fancy working together?
+            <br />
+            <br />
+            <button className="intro-contact-button">Get in touch</button>
+          </p>
+        </div>
+        <div />
       </div>
     </div>
   );
